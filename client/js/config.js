a22bc079d1940a8e704367bef9f930c2115af63d
Erick Berry <berryerick@gmail.com>
2016-06-03 07:22:58 -0700

    working responsive menu

--- client/js/config.js
+++ client/js/config.js
@@ -1,10 +1,21 @@
 var portfolio = angular.module('portfolio', ['ngMaterial', 'ngRoute'])
 
-portfolio.config(function($routeProvider){
+portfolio.config(function($routeProvider, $mdIconProvider, $mdThemingProvider){
+  $mdIconProvider.icon('menu', './img/bars.svg', 200)
+  $mdIconProvider.icon('linkedin', './img/linkedin.svg', 200)
+
+  $mdThemingProvider.theme('default')
+    .primaryPalette('grey')
+    .accentPalette('teal')
+    // .dark()
+
 
   $routeProvider
   .when('/about',{
     templateUrl: "/partials/about.html"
+  })
+  .when('/design',{
+    templateUrl: "/partials/design.html"
   })
   .otherwise({
     redirectTo: '/about'
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,6 +1,27 @@
-portfolio.controller('aboutController', function(projectFactory){
+portfolio.controller('mainController', function(projectFactory, $mdMedia, $mdSidenav){
+  console.log('in mainController')
+  var that = this
+  this.toggleMenu = function(){
+    $mdSidenav('nav').toggle()
+
+    console.log("HELLO?");
+  }
+  // this.$watch(function(){return $mdMedia('ls');} function(big){
+  //   that.bigScreen = big;
+  // })
+  // this.screenIsSmall = $mdMedia('sm')
+  this.errors = []
+
+})
+
+portfolio.controller('aboutController', function(projectFactory, $mdMedia){
   console.log('in aboutController')
   var that = this
+
+  // this.$watch(function(){return $mdMedia('ls');} function(big){
+  //   that.bigScreen = big;
+  // })
+  // this.screenIsSmall = $mdMedia('sm')
   this.errors = []
 
 })
