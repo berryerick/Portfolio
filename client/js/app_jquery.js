ae17d59b250b8916cca054d11db2670284fdfd5e
Erick Berry <berryerick@gmail.com>
2016-06-08 08:54:12 -0700

    icons updated

--- client/js/app_jquery.js
+++ client/js/app_jquery.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   // Initialize collapse button
   $(".button-collapse").sideNav({
-    closeOnClick: true,
-
-  });
+    closeOnClick: true
+  })
+  $(".sidenav .nav-button#nav"+location.hash.slice(2)).addClass("active")
+  console.log(location.hash.slice(2));
   $(".sidenav .nav-button").click(function(){
     $(".sidenav .nav-button").removeClass("active")
-    $(this).addClass("active")
   })
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
